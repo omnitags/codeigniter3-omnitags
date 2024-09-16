2efa0b8840f5c09fff42353eb17be9b2b4b055c8
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2024-09-16 19:42:50 +0700

    Update admin.php

--- application/views/_contents/tabel_b6/admin.php
+++ application/views/_contents/tabel_b6/admin.php
@@ -1,6 +1,6 @@
 <div class="row mb-2 align-items-center">
   <div class="col-md-9 d-flex align-items-center">
-    <h1><?= $title ?><br><span class="h6"> Unread: <?= $notif_count ?></span><?= $phase ?></h1>
+    <h1><?= $title ?><?= count_data($tbl_b6) ?><?= $phase ?></h1>
   </div>
   <div class="col-md-3 text-right">
     <?php foreach ($dekor->result() as $dk): ?>
@@ -9,10 +9,38 @@
   </div>
 </div>
 <hr>
+<!-- tabel fiter pesanan -->
+<table class="mb-4">
 
+  <!-- method get supaya nilai dari filter bisa tampil nanti -->
+  <form action="<?= site_url($language . '/' . $tabel_b6 . '/admin') ?>" method="get">
+    <tr>
+
+      <td class="pr-2">
+        <?= select_edit(
+          'tabel_b6_field7',
+          $tabel_b6_field7_value,
+          $tbl_b7,
+          $tabel_b7_field1,
+          $tabel_b7_field2,
+          'required'
+        ); ?>
+      </td>
+
+      <td>
+        <?= btn_cari() ?>
+        <?= btn_redo('tabel_b6', '/admin') ?>
+      </td>
+    </tr>
+  </form>
+</table>
 
 <div class="row">
   <div class="col-md-10">
+    <?= btn_tambah() ?>
+    <?= btn_laporan('tabel_b6') ?>
+
+
   </div>
 
   <div class="col-md-2 d-flex justify-content-end">
@@ -21,36 +49,32 @@
 </div>
 
 
+
 <div id="card-view" class="row data-view active">
-  <?php if (empty($tbl_b9->result())) {
+  <?php if (empty($tbl_b6->result())) {
     load_view('_partials/no_data');
   } else {
-    foreach ($tbl_b9->result() as $tl_b9):
-      if ($tl_b9->$tabel_b9_field6 == NULL) {
-        echo card_regular(
-          $tl_b9->$tabel_b9_field1,
-          $tl_b9->$tabel_b8_field3,
-          $tl_b9->$tabel_b9_field4 . '<br>' .
-          $tl_b9->$tabel_b9_field5,
-          btn_value('tabel_b9', $tl_b9->$tabel_b9_field1, 'lihat', '<i class="fas fa-envelope-open"></i>') .
-          btn_lihat($tl_b9->$tabel_b9_field1),
-          'text-dark bg-white',
-          'col-md-3',
-          $tabel_b9,
-        );
-      } else {
-        echo card_regular(
-          $tl_b9->$tabel_b9_field1,
-          $tl_b9->$tabel_b8_field3,
-          $tl_b9->$tabel_b9_field4 . '<br>' .
-          $tl_b9->$tabel_b9_field5,
-          btn_lihat($tl_b9->$tabel_b9_field1),
-          'text-dark bg-light',
-          'col-md-3',
-          $tabel_b9,
-        );
+    foreach ($tbl_b6->result() as $tl_b6): ?>
+      <?php
+      $btn_class = '';
+      if ($tl_b6->$tabel_b6_field6 == $tabel_b6_field6_value1) {
+        $btn_class = btn_action('tabel_b6', 'nonaktifkan',  $tl_b6->$tabel_b6_field1, '<i class="fas fa-toggle-on fa-lg"></i>', 'text-warning');
+      } elseif ($tl_b6->$tabel_b6_field6 == $tabel_b6_field6_value2) {
+        $btn_class = btn_action('tabel_b5', 'aktifkan',  $tl_b6->$tabel_b6_field1, '<i class="fas fa-toggle-off fa-lg"></i>', 'text-warning');
       }
-    endforeach;
+      echo card_regular(
+        $tl_b6->$tabel_b6_field1,
+        $tl_b6->$tabel_b6_field2,
+        '<a class="text-decoration-none text-warning" href="' . $tl_b6->$tabel_b6_field4 . '" target="_blank">' . $tl_b6->$tabel_b6_field3 . '</a>' . $btn_class,
+        btn_lihat($tl_b6->$tabel_b6_field1) . ' ' .
+        btn_edit($tl_b6->$tabel_b6_field1) . ' ' .
+        btn_hapus('tabel_b6', $tl_b6->$tabel_b6_field1),
+        'text-white bg-secondary',
+        'col-md-3',
+        $tabel_b6,
+      );
+      ?>
+    <?php endforeach;
   } ?>
 </div>
 
@@ -60,60 +84,145 @@
     <thead class="thead-light">
       <tr>
         <th><?= lang('no') ?></th>
-        <th><?= lang('tabel_b8_field3_alias') ?></th>
-        <th><?= lang('tabel_b9_field4_alias') ?></th>
-        <th><?= lang('tabel_b9_field5_alias') ?></th>
+        <th><?= lang('tabel_b6_field1_alias') ?></th>
+        <th><?= lang('tabel_b6_field2_alias') ?></th>
+        <th><?= lang('tabel_b6_field3_alias') ?></th>
+        <th><?= lang('tabel_b6_field4_alias') ?></th>
+        <th><?= lang('tabel_b6_field5_alias') ?></th>
+        <th><?= lang('tabel_b6_field6_alias') ?></th>
+        <th><?= lang('tabel_b6_field7_alias') ?></th>
         <th><?= lang('action') ?></th>
       </tr>
     </thead>
 
     <tbody>
-      <?php foreach ($tbl_b9->result() as $tl_b9):
-        if ($tl_b9->$tabel_b9_field6 == NULL) { ?>
-          <tr class="bg-white">
-            <td></td>
-            <td><?= $tl_b9->$tabel_b8_field3 ?></td>
-            <td><?= $tl_b9->$tabel_b9_field4 ?></td>
-            <td><?= $tl_b9->$tabel_b9_field5 ?></td>
-            <td>
-              <?= btn_value('tabel_b9', $tl_b9->$tabel_b9_field1, 'lihat', '<i class="fas fa-envelope-open"></i>') ?>
-              <?= btn_lihat($tl_b9->$tabel_b9_field1) ?>
-            </td>
-          </tr>
-        <?php } else { ?>
-          <tr class="bg-light">
-            <td></td>
-            <td><?= $tl_b9->$tabel_b8_field3 ?></td>
-            <td><?= $tl_b9->$tabel_b9_field4 ?></td>
-            <td><?= $tl_b9->$tabel_b9_field5 ?></td>
-            <td>
-              <?= btn_lihat($tl_b9->$tabel_b9_field1) ?>
-            </td>
-          </tr>
-        <?php }endforeach; ?>
+      <?php foreach ($tbl_b6->result() as $tl_b6): ?>
+        <tr>
+          <td></td>
+          <td><?= $tl_b6->$tabel_b6_field1; ?></td>
+          <td><?= $tl_b6->$tabel_b6_field2 ?></td>
+          <td><?= $tl_b6->$tabel_b6_field3 ?></td>
+          <td><a href="<?= $tl_b6->$tabel_b6_field4 ?>" target="_blank">Visit</a>
+          <td>
+            <h2><?= $tl_b6->$tabel_b6_field5 ?></h2>
+          </td>
+          <td>
+            <?php if ($tl_b6->$tabel_b6_field6 == $tabel_b6_field6_value1) { ?>
+              <?= btn_action('tabel_b6', 'nonaktifkan',  $tl_b6->$tabel_b6_field1, '<i class="fas fa-toggle-on fa-lg"></i>', 'text-warning') ?>
+
+            <?php } elseif ($tl_b6->$tabel_b6_field6 == $tabel_b6_field6_value2) { ?>
+              <?= btn_action('tabel_b6', 'aktifkan',  $tl_b6->$tabel_b6_field1, '<i class="fas fa-toggle-off fa-lg"></i>', 'text-warning') ?>
+
+            <?php } else { ?>
+
+            <?php } ?>
+          </td>
+          <td><?= $tl_b6->$tabel_b6_field7 ?></td>
+          <td><?= btn_lihat($tl_b6->$tabel_b6_field1) ?>
+            <?= btn_edit($tl_b6->$tabel_b6_field1) ?>
+            <?= btn_hapus('tabel_b6', $tl_b6->$tabel_b6_field1) ?>
+          </td>
+        </tr>
+
+      <?php endforeach; ?>
     </tbody>
 
 
   </table>
 </div>
 
+<!-- modal tambah -->
+<div id="tambah" class="modal fade tambah">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <?= modal_header(lang('add') . ' ' . lang('tabel_b6_alias'), '') ?>
 
-<!-- modal lihat -->
-<?php foreach ($tbl_b9->result() as $tl_b9): ?>
-  <div id="lihat<?= $tl_b9->$tabel_b9_field1; ?>" class="modal fade lihat" role="dialog">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+      <form action="<?= site_url($language . '/' . $tabel_b6 . '/tambah') ?>" enctype="multipart/form-data"
+        method="post">
+        <div class="modal-body">
+          <?= input_add('text', 'tabel_b6_field2', 'required') ?>
+          <?= input_add('text', 'tabel_b6_field3', 'required') ?>
+          <?= input_add('text', 'tabel_b6_field4', 'required') ?>
+          <?= input_add('text', 'tabel_b6_field5', 'required') ?>
+
+          <?= select_add(
+            'tabel_b6_field7',
+            $tbl_b7,
+            $tabel_b7_field1,
+            $tabel_b7_field2,
+            'required'
+          ); ?>
+
+        </div>
+
+        <!-- memunculkan notifikasi modal -->
+        <p class="small text-center text-danger"><?= get_flashdata('pesan_tambah') ?></p>
+
+        <div class="modal-footer">
+          <?= btn_simpan() ?>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+<!-- modal edit-->
+<?php foreach ($tbl_b6->result() as $tl_b6): ?>
+  <div id="ubah<?= $tl_b6->$tabel_b6_field1; ?>" class="modal fade ubah">
+    <div class="modal-dialog">
       <div class="modal-content">
-        <?= modal_header_id(lang('tabel_b9_alias'), $tl_b9->$tabel_b9_field1) ?>
+        <?= modal_header_id(lang('change_data') . ' ' . lang('tabel_b6_alias'), $tl_b6->$tabel_b6_field1) ?>
+
+        <form action="<?= site_url($language . '/' . $tabel_b6 . '/update') ?>" method="post"
+          enctype="multipart/form-data">
+          <div class="modal-body">
+
+            <?= input_hidden('tabel_b6_field1', $tl_b6->$tabel_b6_field1, 'required') ?>
+            <?= input_edit('text', 'tabel_b6_field2', $tl_b6->$tabel_b6_field2, 'required') ?>
+            <?= input_edit('text', 'tabel_b6_field3', $tl_b6->$tabel_b6_field3, 'required') ?>
+            <?= input_edit('text', 'tabel_b6_field4', $tl_b6->$tabel_b6_field4, 'required') ?>
+            <?= input_edit('text', 'tabel_b6_field5', htmlspecialchars($tl_b6->$tabel_b6_field5), 'required') ?>
+
+            <?= select_edit(
+              'tabel_b6_field7',
+              $tl_b6->$tabel_b6_field7,
+              $tbl_b7,
+              $tabel_b7_field1,
+              $tabel_b7_field2,
+              'required'
+            ); ?>
+
+          </div>
+
+          <!-- memunculkan notifikasi modal -->
+          <p class="small text-center text-danger"><?= get_flashdata('pesan_ubah') ?></p>
+
+          <div class="modal-footer">
+            <?= btn_update() ?>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+  <div id="lihat<?= $tl_b6->$tabel_b6_field1; ?>" class="modal fade lihat" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <?= modal_header_id(lang('tabel_b6_alias'), $tl_b6->$tabel_b6_field1) ?>
 
         <!-- administrator tidak bisa melihat password user lain -->
         <form>
           <div class="modal-body">
             <?= table_data(
-              row_data('tabel_b8_field3', $tl_b9->$tabel_b8_field3) .
-              row_data('tabel_b9_field4', html_entity_decode($tl_b9->$tabel_b9_field4)) .
-              row_data('tabel_b9_field5', $tl_b9->$tabel_b9_field5) .
-              row_data('tabel_b9_field6', $tl_b9->$tabel_b9_field6),
-              'table-light',
+              row_data('tabel_b6_field2', $tl_b6->$tabel_b6_field2) .
+              row_data('tabel_b6_field3', $tl_b6->$tabel_b6_field3) .
+              row_data('tabel_b6_field4', '<a class="text-decoration-none text-warning" href="' . $tl_b6->$tabel_b6_field4 . '" target="_blank">' . $tl_b6->$tabel_b6_field3 . '</a>') .
+              row_data('tabel_b6_field5', $tl_b6->$tabel_b6_field5) .
+              row_data('tabel_b6_field6', $tl_b6->$tabel_b6_field6) .
+              row_data('tabel_b6_field7', $tl_b6->$tabel_b6_field7),
+              'table-light'
             ) ?>
           </div>
 
@@ -129,3 +238,5 @@
     </div>
   </div>
 <?php endforeach; ?>
+
+<?= adjust_col_js() ?>
