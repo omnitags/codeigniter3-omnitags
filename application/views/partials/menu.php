a23a70404ffd507e4a0e79ecd2b2438f8d3cfa79
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2024-10-11 21:30:49 +0700

    Fix menu and postman

--- application/views/partials/menu.php
+++ application/views/partials/menu.php
@@ -40,18 +40,9 @@
                                     case $tabel_c2_field6_value3:
                                         ?>
                                         <h6 class="dropdown-header">Data</h6>
-                                        <?= dropdown_nav('tabel_e2', '/admin') ?>
-                                        <?= dropdown_nav('tabel_e3', '/admin') ?>
                                         <?= dropdown_nav('tabel_e4', '/admin') ?>
                                         <div class="dropdown-divider"></div>
-                                        <h6 class="dropdown-header">Manage</h6>
-                                        <?= dropdown_nav('tabel_e5', '/admin') ?>
-                                        <?= dropdown_nav('tabel_e6', '/admin') ?>
-                                        <?= dropdown_nav('tabel_e7', '/admin') ?>
-                                        <?= dropdown_nav('tabel_e8', '/admin') ?>
-                                        <div class="dropdown-divider"></div>
                                         <h6 class="dropdown-header">Operations</h6>
-                                        <?= dropdown_nav('tabel_e1', '/admin') ?>
                                         <?= dropdown_nav('tabel_c1', '/admin') ?>
                                         <?= dropdown_nav('tabel_c2', '/admin') ?>
                                         <div class="dropdown-divider"></div>
