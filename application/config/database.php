f7c37671cfa2210efdfb217e13ece41469fbbd51
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2024-12-23 12:58:04 +0700

    Fix the error, and fix the setup

--- application/config/database.php
+++ application/config/database.php
@@ -1,4 +1,3 @@
-@ -0,0 +1,97 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
@@ -79,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'college',
+	'database' => 'me_college',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
