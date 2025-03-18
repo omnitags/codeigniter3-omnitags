3ad576352063ac42fbc82433e90faafc5458dffd
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2025-03-18 08:06:59 +0700

    Init hotel

--- application/config/database.php
+++ application/config/database.php
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'school_ukk_hotel',
+	'database' => 'me_school_ukk_hotel',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -93,5 +93,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);	
-?>
+);
